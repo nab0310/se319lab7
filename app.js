cd916d00da2d10b33d189863eaa1d72702b6d796
Nick Behrens <behrensn@iastate.edu>
2016-11-14 12:59:01 -0600

    Got the table display working, working on getting an add buttongoing.

--- app.js
+++ app.js
@@ -39,10 +39,15 @@ var app = angular.module('myApp', ['ngRoute']); //ngRoute is an angular service
 
     app.service('libraryService', function(){
         this.books = [];
-        this.books[0] = {name: 'book1', id: '1'};
-        this.books[1] = {name: 'book2', id: '2'};
-        this.books[2] = {name: 'book3', id: '3'};
-        this.books[3] = {name: 'book4', id: '4'};
+        this.books[0] = {name: 'book1', id: '1', borrowedBy: '', presence:'1'};
+        this.books[1] = {name: 'book2', id: '2', borrowedBy: '', presence:'1'};
+        this.books[2] = {name: 'book3', id: '3', borrowedBy: '', presence:'1'};
+        this.books[3] = {name: 'book4', id: '4', borrowedBy: '', presence:'1'};
+        this.books[4] = {name: 'book5', id: '5', borrowedBy: '', presence:'1'};
+        this.books[5] = {name: 'book6', id: '6', borrowedBy: '', presence:'1'};
+        this.books[6] = {name: 'book7', id: '7', borrowedBy: '', presence:'1'};
+        this.books[7] = {name: 'book8', id: '8', borrowedBy: '', presence:'1'};
+
     });
 
     app.controller('studentController', function ($scope, libraryService) {
@@ -60,4 +65,22 @@ var app = angular.module('myApp', ['ngRoute']); //ngRoute is an angular service
         $scope.bookClick = function() {
             $scope.message = "Librarian Click!";
         };
+        $scope.splitData = splitIntoRows(libraryService.books,4);
+
+        $scope.addBook = function() {
+            libraryService.books.push({name: $scope.bookName,id:$scope.Shelf,borrowedBy:'',presence:'1'});
+        };
+
+        function splitIntoRows(items, itemsPerRow) {
+            var rslt = [];
+            items.forEach(function(item, index) {
+                var rowIndex = Math.floor(index / itemsPerRow),
+                    colIndex = item.id % itemsPerRow;
+                if (!rslt[rowIndex]) {
+                    rslt[rowIndex] = [];
+                }
+                rslt[rowIndex][colIndex] = item;
+            });
+            return rslt;
+        }
     });
