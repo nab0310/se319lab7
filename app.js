2b6ff9e061d28f5c076c6a992301763a4ce6312e
Nick Behrens <behrensn@iastate.edu>
2016-11-14 18:35:42 -0600

    Getting on click to work

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var app = angular.module('myApp', ['ngRoute']); //ngRoute is an angular service
             if(i%4==3){
                 column = "Art";
             }
-            $rootScope.books.push({name:'B'+i,shelf:column,borrowedBy:'',presence:'1'});
+            $rootScope.books.push({name:'B'+i,bookType: 'ordinary', shelf:column, borrowedBy:'',presence:'1'});
         }
         for(j=0;j<5;j++){
             if(j%4==0){
@@ -48,7 +48,7 @@ var app = angular.module('myApp', ['ngRoute']); //ngRoute is an angular service
             if(j%4==3){
                 column = "Art";
             }
-            $rootScope.books.push({name:'R'+j,shelf:column,borrowedBy:'',presence:'0'});
+            $rootScope.books.push({name:'R'+j, bookType: 'reference', shelf:column,borrowedBy:'',presence:'0'});
         }
         $scope.name = "IndexView";
         $scope.validate = function () {
@@ -69,18 +69,6 @@ var app = angular.module('myApp', ['ngRoute']); //ngRoute is an angular service
         }
     });
 
-    app.service('libraryService', function(){
-        this.books = [];
-        this.books[0] = {name: 'book1', shelf: 'Literature', borrowedBy: '', presence:'1'};
-        this.books[1] = {name: 'book2', shelf: 'Science', borrowedBy: '', presence:'1'};
-        this.books[2] = {name: 'book3', shelf: 'Sport', borrowedBy: '', presence:'1'};
-        this.books[3] = {name: 'book4', shelf: 'Art', borrowedBy: '', presence:'1'};
-        this.books[4] = {name: 'book5', shelf: 'Literature', borrowedBy: '', presence:'1'};
-        this.books[5] = {name: 'book6', shelf: 'Science', borrowedBy: '', presence:'1'};
-        this.books[6] = {name: 'book7', shelf: 'Sport', borrowedBy: '', presence:'1'};
-        this.books[7] = {name: 'book8', shelf: 'Art', borrowedBy: '', presence:'1'};
-    });
-
     app.controller('studentController', function ($scope, $rootScope) {
         $scope.name = "Student";
         $scope.library = $rootScope.books;
@@ -93,12 +81,17 @@ var app = angular.module('myApp', ['ngRoute']); //ngRoute is an angular service
         $scope.name = "Librarian";
         $scope.library = $rootScope.books;
         $scope.message = "Nothing clicked.";
-        $scope.bookClick = function() {
+        $scope.bookClick = function(name,type,borrowedBy,presence) {
+            alert("Name: "+name+", Type: "+type+", BorrowedBy: "+borrowedBy+", presence: "+presence);
             $scope.message = "Librarian Click!";
         };
 
         $scope.addRow = function(){
-            $rootScope.books.push({name:$scope.bookName, shelf: $scope.Shelf, borrowedBy:'', presence: '1' });
+            if($scope.Reference==1){
+                $rootScope.books.push({name:$scope.bookName, bookType: 'reference', shelf: $scope.Shelf, borrowedBy:'', presence: '0' });
+            }else {
+                $rootScope.books.push({name: $scope.bookName, bookType: 'ordinary', shelf: $scope.Shelf, borrowedBy: '', presence: '1'});
+            }
         };
 
     });
